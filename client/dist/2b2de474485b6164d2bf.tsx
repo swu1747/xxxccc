634b03f1750e139f159cf9d9ac6bd7785767c80d
swu1747 <swu1747@gmail.com>
2025-02-07 15:22:55 -0500

    save

---
+++ client/dist/2b2de474485b6164d2bf.tsx
@@ -0,0 +1,5 @@
+import App from './MyApp/app';
+import React from 'react';
+import ReactDOM from "react-dom/client";
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(App, null));
---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import App from './MyApp/app'
+import React from 'react'
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
